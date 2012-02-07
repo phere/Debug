1bf63451ba604aaf40499d8f4387e8945f784fd7
Rowan James <rowan.james@asdeqlabs.com>
2012-02-07 13:28:21 +1100

    Assert.hpp - fix not-disabling bug and add a lot of comments

--- Assert.hpp
+++ Assert.hpp
@@ -6,8 +6,7 @@
 //  Copyright 2012 Asdeq Labs. All rights reserved.
 //
 
-
-#define BOOST_ENABLE_ASSERT_HANDLER
+////////
 namespace boost {
 	void assertion_failed(char const*	expr,
 						  char const*	function,
@@ -19,7 +18,29 @@ namespace boost {
 							  char const*	file,
 							  long 			line);
 }
+
+////////
+#define BOOST_ENABLE_ASSERT_HANDLER
+// This does not enable BOOST_ASSERT et. al., but rather directs it to call
+// ::boost::asertion_failed( ... ) declared above instead of `abort()`
+// This must be defined before <boost/assert.hpp> is included to take effect.
+// Do keep in mind that returning from assertion_failed is likely to result
+// in effects like getting SIGSEGV instead of SIGABRT.
+
+////////
+#if defined(NDEBUG)
+#define BOOST_DISABLE_ASSERTS
+#endif
+// Explicitly disable BOOST_ASSERT if NDEBUG is defined.
+// A side effect in Boost 1.48.0 effectively evaluates the expression if
+// BOOST_ENABLE_ASSERT_HANDLER is defined, even if the assertion is not
+// to be checked.
+// We want BOOST_ASSERT to track `assert()` anyway, so this just ensures that.
+
 #import <boost/assert.hpp>
+
+////////
+// Define the 'standard' shorthand macros for ASSERT/VERIFY
 
 // ASSERT(expr);
 #define ASSERT BOOST_ASSERT
