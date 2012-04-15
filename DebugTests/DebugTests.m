0789e1e70484ad86890c4f8b229c19312f6134ba
Rowan James <rowanj@phere.net>
2012-04-15 16:35:25 +1000

    Create an Xcode project file for Phere Debug

---
+++ DebugTests/DebugTests.m
@@ -0,0 +1,32 @@
+//
+//  DebugTests.m
+//  DebugTests
+//
+//  Created by Rowan James on 15/04/2012.
+//  Copyright (c) 2012 Phere Development Pty. Ltd. All rights reserved.
+//
+
+#import "DebugTests.h"
+
+@implementation DebugTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DebugTests");
+}
+
+@end
