512877c329266fd1a3d6e68c4aa51f7d3d3e94e3
Rowan James <rowanj@phere.net>
2012-09-30 20:09:24 +1000

    Add fallback for isDebugging for non-Apple platforms

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -12,26 +12,32 @@
 #ifdef __APPLE__
 #include <sys/sysctl.h>
 #include <unistd.h>
+#endif
 
 namespace phere {
-	namespace debug {
-		bool IsDebugging() {
-			static bool result(detail::IsDebugging());
-			return result;
-		}
+  namespace debug {
+    bool IsDebugging() {
+      static bool result(detail::IsDebugging());
+      return result;
+    }
 		
-		namespace detail {
-			bool IsDebugging() {
-				int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid () };
-				struct kinfo_proc info;
-				memset(&info, 0, sizeof(info));
-				size_t size = sizeof (info);
-				sysctl (mib, sizeof (mib) / sizeof (*mib), &info, &size, NULL, 0);
+    namespace detail {
+#ifdef __APPLE__
+      bool IsDebugging() {
+	int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid () };
+	struct kinfo_proc info;
+	memset(&info, 0, sizeof(info));
+	size_t size = sizeof (info);
+	sysctl (mib, sizeof (mib) / sizeof (*mib), &info, &size, NULL, 0);
 				
-				// We're being debugged if the P_TRACED flag is set.
-				return (info.kp_proc.p_flag & P_TRACED) != 0;
-			}
-		}
-	}
-}
+	// We're being debugged if the P_TRACED flag is set.
+	return (info.kp_proc.p_flag & P_TRACED) != 0;
+      }
+#else
+      bool isDebugging() {
+	return false;
+      }
 #endif
+    }
+  }
+}
